45884db3cb146d7898f03b650ee5c4cf67aea2b4
devnoname120 <devnoname120@gmail.com>
2020-12-19 21:52:28 +0100

    Fix bugs and add selector

--- main.c
+++ main.c
@@ -6,11 +6,20 @@
 
 #include <vita2d.h>
 
+#define LUT_SIZE (357)
+#define COLOR_WHITE (RGBA8(255, 255, 255, 255))
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+int vitabrightReload();
+int vitabrightGetOledLut(unsigned char oledLut[LUT_SIZE]);
+int vitabrightSetOledLut(unsigned char oledLut[LUT_SIZE]);
+
 int main() {
   vita2d_pgf *pgf;
   vita2d_pvf *pvf;
   vita2d_texture *image;
-  float rad = 0.0f;
 
   vita2d_init();
   vita2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
@@ -18,18 +27,46 @@ int main() {
   pgf = vita2d_load_default_pgf();
   pvf = vita2d_load_default_pvf();
 
+  unsigned char oledLut[LUT_SIZE] = {0};
+  int ret = vitabrightGetOledLut(oledLut);
+
+  // Wrong vitabright version installed.
+  if (ret != 0) {
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+    while (1) {
+      vita2d_start_drawing();
+      vita2d_clear_screen();
+      vita2d_pgf_draw_text(pgf,
+                           5,
+                           30,
+                           COLOR_WHITE,
+                           1.15f,
+                           "Cannot communicate with vitabright. Make sure that vitabright v1.1 or "
+                           "later is installed.");
+
+      vita2d_end_drawing();
+      vita2d_swap_buffers();
+    }
+#pragma clang diagnostic pop
+  }
+
   /*
    * Load the statically compiled image.png file.
    */
 
   vita2d_texture *textures[] = {vita2d_load_PNG_file("app0:/resources/colorbars.png"),
-                               vita2d_load_PNG_file("app0:/resources/matrix.png"),
-                               vita2d_load_JPEG_file("app0:/resources/tv.jpg")};
+                                vita2d_load_PNG_file("app0:/resources/matrix.png"),
+                                vita2d_load_JPEG_file("app0:/resources/tv.jpg")};
 
   unsigned int textureIndex = 0;
 
   SceCtrlData pad = {0};
   SceCtrlData oldPad = {0};
+
+  // FIXME: sceDisplayGetBrightness.
+  int currentLevel = 4;
+  int curEditGroup = 0;
 
   while (1) {
     sceCtrlPeekBufferPositive(0, &pad, 1);
@@ -38,13 +75,55 @@ int main() {
       break;
 
     if ((pad.buttons & SCE_CTRL_CROSS) && !(oldPad.buttons & SCE_CTRL_CROSS))
-      textureIndex = (textureIndex + 1) % (sizeof(textures)/sizeof(*textures));
+      textureIndex = (textureIndex + 1) % (sizeof(textures) / sizeof(*textures));
+
+    if ((pad.buttons & SCE_CTRL_SQUARE) && !(oldPad.buttons & SCE_CTRL_SQUARE)) {
+      currentLevel = MIN(currentLevel + 1, 16);
+    }
+
+    if ((pad.buttons & SCE_CTRL_TRIANGLE) && !(oldPad.buttons & SCE_CTRL_TRIANGLE)) {
+      currentLevel = MAX(currentLevel - 1, 0);
+    }
+
+    if ((pad.buttons & SCE_CTRL_LEFT) && !(oldPad.buttons & SCE_CTRL_LEFT)) {
+      curEditGroup = MAX(curEditGroup - 1, 0);
+    }
+
+    if ((pad.buttons & SCE_CTRL_RIGHT) && !(oldPad.buttons & SCE_CTRL_RIGHT)) {
+      curEditGroup = MIN(curEditGroup + 1, 20);
+    }
 
     vita2d_start_drawing();
     vita2d_clear_screen();
 
     vita2d_draw_texture(textures[textureIndex], 0, 0);
-    vita2d_pgf_draw_text(pgf, 120, 500, RGBA8(255, 255, 255, 255), 1.0f, "E1 E1 FF CF D7 CA C1 C9 BA E1 E3 DE D5 CF D3 FA ED E6 2F 00 2F");
+
+    int x = 120;
+
+    char editing[30] = {0};
+    sprintf(editing, "Editing: %u", currentLevel);
+    vita2d_pgf_draw_text(pgf, 0, 500, COLOR_WHITE, 1.0f, editing);
+
+    for (int i = 0; i < 21; ++i) {
+      char value[3] = {0};
+      sprintf(value, "%02X", (char)oledLut[currentLevel * 21 + i]);
+
+      vita2d_pgf_draw_text(pgf, x, 500, COLOR_WHITE, 1.0f, value);
+
+      int width = 0;
+      int height = 0;
+      vita2d_pgf_text_dimensions(pgf, 1.0f, value, &width, &height);
+
+      if (i == curEditGroup) {
+        vita2d_draw_rectangle(x, 500 + height / 2, width, 5, COLOR_WHITE);
+      }
+
+      x += width + 10;
+    }
+
+    char active[30] = {0};
+    sprintf(active, "Actual brightness level: %u", 7);
+    vita2d_pgf_draw_text(pgf, 0, 540, COLOR_WHITE, 1.0f, active);
 
     vita2d_end_drawing();
     vita2d_swap_buffers();
@@ -53,7 +132,10 @@ int main() {
   }
 
   vita2d_fini();
-  vita2d_free_texture(image);
+
+  for (int i = 0; i < sizeof(textures) / sizeof(*textures); ++i) {
+    vita2d_free_texture(textures[i]);
+  }
   vita2d_free_pgf(pgf);
   vita2d_free_pvf(pvf);
 
