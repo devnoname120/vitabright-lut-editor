cfe3dd0467e8cff3c4260f12fab7bca13f1a4a3a
devnoname120 <devnoname120@gmail.com>
2020-12-20 10:26:05 +0100

    Add LUT edit and write back

--- main.c
+++ main.c
@@ -12,23 +12,74 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-int vitabrightReload();
-int vitabrightGetOledLut(unsigned char oledLut[LUT_SIZE]);
-int vitabrightSetOledLut(unsigned char oledLut[LUT_SIZE]);
+#define LUT_FILE_1 ("ur0:/tai/vitabright_lut.txt")
+#define LUT_FILE_2 ("ux0:/tai/vitabright_lut.txt")
+static const int DECREMENT_FAST_THRESHOLD = 300 * 1000;
+
+int vitabrightOledGetLevel();
+int vitabrightOledSetLevel(unsigned int level);
+int vitabrightOledGetLut(unsigned char oledLut[LUT_SIZE]);
+int vitabrightOledSetLut(unsigned char oledLut[LUT_SIZE]);
+
+int currentLevel = 0;
+int screenLevel = 0;
+
+int curEditGroup = 0;
+
+unsigned char oledLut[LUT_SIZE] = {0};
+uint32_t lastGroupStartEditTime = 0;
+
+void changeLevel(int isStart, int increment) {
+  if (!isStart && (sceKernelGetProcessTimeLow() - lastGroupStartEditTime) < DECREMENT_FAST_THRESHOLD)
+    return;
+
+  if (isStart) {
+    lastGroupStartEditTime = sceKernelGetProcessTimeLow();
+  }
+
+  oledLut[currentLevel * 21 + curEditGroup] = MAX(MIN(((int)oledLut[currentLevel * 21 + curEditGroup]) + increment, 255), 0);
+  vitabrightOledSetLut(oledLut);
+}
+
+void writeLut() {
+  char *lutFilePath = LUT_FILE_1;
+
+  FILE *readLut = fopen(lutFilePath, "r");
+  if (!readLut) {
+    lutFilePath = LUT_FILE_2;
+  } else {
+    fclose(readLut);
+  }
+
+  FILE *lutFile = fopen(lutFilePath, "w");
+
+  char newLut[2000] = {0};
+  unsigned int position = 0;
+
+  for (int i = 0; i < 17; ++i) {
+    for (int j = 0; j < 20; ++j) {
+      sprintf(&(newLut[position]), "%02X ", oledLut[i * 21 + j]);
+      position += 3;
+    }
+
+    sprintf(&(newLut[position]), "%02X\n", oledLut[i * 21 + 20]);
+    position += 3;
+  }
+
+  fputs(newLut, lutFile);
+  fclose(lutFile);
+}
 
 int main() {
   vita2d_pgf *pgf;
   vita2d_pvf *pvf;
-  vita2d_texture *image;
 
   vita2d_init();
   vita2d_set_clear_color(RGBA8(0x40, 0x40, 0x40, 0xFF));
 
   pgf = vita2d_load_default_pgf();
-  pvf = vita2d_load_default_pvf();
 
-  unsigned char oledLut[LUT_SIZE] = {0};
-  int ret = vitabrightGetOledLut(oledLut);
+  int ret = vitabrightOledGetLut(oledLut);
 
   // Wrong vitabright version installed.
   if (ret != 0) {
@@ -64,15 +115,14 @@ int main() {
   SceCtrlData pad = {0};
   SceCtrlData oldPad = {0};
 
-  // FIXME: sceDisplayGetBrightness.
-  int currentLevel = 4;
-  int curEditGroup = 0;
+  currentLevel = vitabrightOledGetLevel();
+  screenLevel = vitabrightOledGetLevel();
+  uint32_t lastGetLevelTime = sceKernelGetProcessTimeLow();
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
   while (1) {
     sceCtrlPeekBufferPositive(0, &pad, 1);
-
-    if (pad.buttons & SCE_CTRL_START)
-      break;
 
     if ((pad.buttons & SCE_CTRL_CROSS) && !(oldPad.buttons & SCE_CTRL_CROSS))
       textureIndex = (textureIndex + 1) % (sizeof(textures) / sizeof(*textures));
@@ -91,6 +141,33 @@ int main() {
 
     if ((pad.buttons & SCE_CTRL_RIGHT) && !(oldPad.buttons & SCE_CTRL_RIGHT)) {
       curEditGroup = MIN(curEditGroup + 1, 20);
+    }
+
+    if ((pad.buttons & SCE_CTRL_UP)) {
+      int isStart = 0;
+      if (!(oldPad.buttons & SCE_CTRL_UP)) {
+        isStart = 1;
+      }
+
+      changeLevel(isStart, 1);
+    }
+
+    if ((pad.buttons & SCE_CTRL_DOWN)) {
+      int isStart = 0;
+      if (!(oldPad.buttons & SCE_CTRL_DOWN)) {
+        isStart = 1;
+      }
+
+      changeLevel(isStart, -1);
+    }
+
+    if ((pad.buttons & SCE_CTRL_SELECT) && !(oldPad.buttons & SCE_CTRL_SELECT)) {
+      vitabrightOledSetLevel(currentLevel);
+      screenLevel = vitabrightOledGetLevel();
+    }
+
+    if ((pad.buttons & SCE_CTRL_START) && !(oldPad.buttons & SCE_CTRL_START)) {
+      writeLut();
     }
 
     vita2d_start_drawing();
@@ -122,14 +199,19 @@ int main() {
     }
 
     char active[30] = {0};
-    sprintf(active, "Actual brightness level: %u", 7);
+    sprintf(active, "Screen brightness level: %u", screenLevel);
     vita2d_pgf_draw_text(pgf, 0, 540, COLOR_WHITE, 1.0f, active);
 
     vita2d_end_drawing();
     vita2d_swap_buffers();
 
+    if (sceKernelGetProcessTimeLow() - lastGetLevelTime > 500 * 1000) {
+      screenLevel = vitabrightOledGetLevel();
+    }
+
     oldPad = pad;
   }
+#pragma clang diagnostic pop
 
   vita2d_fini();
 
